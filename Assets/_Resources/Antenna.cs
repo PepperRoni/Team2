707f8b4e157ac95a51e0ce4927e6f01666786989
5tien <Sten.bosma00@gmail.com>
2022-02-24 22:15:27 +0100

    Alrighty

--- Assets/_Resources/Antenna.cs
+++ Assets/_Resources/Antenna.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Antenna : MonoBehaviour
 {
     [SerializeField] GameObject preAntennaPrefab;
-    //[SerializeField] GameObject ps;
+    [SerializeField] GameObject ps;
     [SerializeField] GameObject doneAntennaPrefab;
     Animator animator;
     void Start()
@@ -22,17 +22,34 @@ public class Antenna : MonoBehaviour
             StartCoroutine(PlayEndAnimation());
         }
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        PlayerController controller = other.GetComponent<PlayerController>();
+
+        if (controller && controller.collectedItems >= 5)
+        {
+            print("wooo!!!!");
+            StartCoroutine(PlayEndAnimation());
+        }
+        else
+        {
+            print("Not enough parts :(");
+        }
+    }
+
     //4.2
     private IEnumerator PlayEndAnimation()
     {
-        Instantiate(preAntennaPrefab, transform);
+        GameObject antenna = Instantiate(preAntennaPrefab, this.transform.position, Quaternion.Euler(0, 180, 0));
         animator = preAntennaPrefab.GetComponent<Animator>();
         animator.Play("Shatter");
+        yield return new WaitForSeconds(9f);
+        Destroy(antenna);
+        GameObject particle = Instantiate(ps, this.transform.position, Quaternion.identity);
         yield return new WaitForSeconds(5f);
-        Destroy(preAntennaPrefab);
-        //ps.Play();
-        yield return new WaitForSeconds(2f);
-        Instantiate(doneAntennaPrefab, transform);
+        Instantiate(doneAntennaPrefab, this.transform.position, Quaternion.Euler(0, 180, 0));
+        Destroy(particle);
         yield return null;
     }
 }
