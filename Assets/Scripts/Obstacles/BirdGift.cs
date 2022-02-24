58c73e350b5556532354394c57d842078d48f718
LukeR2003 <78425989+LukeR2003@users.noreply.github.com>
2022-02-24 11:58:29 +0100

    bird poopi update

--- Assets/Scripts/Obstacles/BirdGift.cs
+++ Assets/Scripts/Obstacles/BirdGift.cs
@@ -5,22 +5,43 @@ using UnityEngine;
 public class BirdGift : MonoBehaviour
 {
     [SerializeField] private float despawnTime;
+    [SerializeField] private ParticleSystem shartBuster;
+    [SerializeField] private GameObject splatter;
     private float despawnTimer;
-
+    
     void Start()
     {
         despawnTimer = 0;
+        splatter.SetActive(false);
     }
 
     void Update()
     {
         despawnTimer += 1 * Time.deltaTime;
         if (despawnTimer > despawnTime)
-            Destroy(gameObject);
+            StartCoroutine(Explode());
     }
 
     private void OnCollisionEnter(Collision collision)
     {
+        StartCoroutine(Explode());
+    }
+
+    private IEnumerator Explode()
+    {
+        gameObject.GetComponent<MeshRenderer>().enabled = false;
+        gameObject.GetComponent<CapsuleCollider>().enabled = false;
+        gameObject.GetComponent<Rigidbody>().useGravity = false;
+        shartBuster.Play();
+        splatter.SetActive(true);
+        yield return new WaitForSeconds(3);
         Destroy(gameObject);
+    }
+
+    private void OnDrawGizmos()
+    {
+
+        Gizmos.DrawWireSphere(gameObject.transform.position, 2);
+        
     }
 }
